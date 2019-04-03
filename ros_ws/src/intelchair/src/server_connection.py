36935fc927db36a2bd938add00c2624573ab4a9e
Fabio Alves <fabioalves98@ua.pt>
2019-04-03 17:44:37 +0100

    change of requests to python urllib

--- ros_ws/src/intelchair/src/server_connection.py
+++ ros_ws/src/intelchair/src/server_connection.py
@@ -1,7 +1,18 @@
+#!/usr/bin/env python
+
 import netifaces as ni
-import requests
+import urllib
+import urllib2
+import rospy
 
-SERVER = '127.0.0.1'
-ip = ni.ifaddresses('wlo1')[ni.AF_INET][0]['addr']
-r = requests.post(url = SERVER, data={'chair_ip': ip})
+rospy.init_node("server_connection", anonymous=True)
 
+ip = ni.ifaddresses('wlp3s0')[ni.AF_INET][0]['addr']
+
+url = 'http://127.0.0.1:8080/api/setip'
+values = {'chair_ip' : ip}
+data = urllib.urlencode(values)
+req = urllib2.Request(url)
+req.add_data(data)
+    
+html = urllib2.urlopen(req)
