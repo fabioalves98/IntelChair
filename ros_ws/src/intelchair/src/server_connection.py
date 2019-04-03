c042684f09601469abf9a147354461132211cbb5
Fabio Alves <fabioalves98@ua.pt>
2019-04-03 18:42:08 +0100

    Server + Mobile + Chair integration

--- ros_ws/src/intelchair/src/server_connection.py
+++ ros_ws/src/intelchair/src/server_connection.py
@@ -9,7 +9,8 @@ rospy.init_node("server_connection", anonymous=True)
 
 ip = ni.ifaddresses('wlp3s0')[ni.AF_INET][0]['addr']
 
-url = 'http://127.0.0.1:8080/api/setip'
+#url = 'http://127.0.0.1:8080/api/setip'
+url = 'http://192.168.1.209:8080/api/setip'
 values = {'chair_ip' : ip}
 data = urllib.urlencode(values)
 req = urllib2.Request(url)
