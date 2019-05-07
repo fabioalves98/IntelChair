86c94cae0e8d71754b4034e877cf35b629a26be2
Diogo Marques <d.marques@ua.pt>
2019-05-07 01:20:29 +0100

    start login comunication with server
    Former-commit-id: b31c4e69910b7a8d3b179ef637e1ba415a54fa35

--- server/intelchair/static/joystick.js
+++ server/intelchair/static/joystick.js
@@ -8,7 +8,7 @@ var options = {
     size: '100',
     position: {left: '50%', bottom: '50%'},
     mode: 'static'
-};	
+};
 var manager = nipplejs.create(options);
 var joystick = {x : 0, y : 0};
 var currentSpeed = 1;
@@ -40,7 +40,7 @@ setInterval(function(){
 		});
 		// var twist = new ROSLIB.Message({
 		// 	linear:{
-		// 		x: 
+		// 		x:
 		// 		y: 0
 		// 		z: 0
 		// 	},
@@ -56,6 +56,11 @@ setInterval(function(){
 
 }, 50);
 
+
+function validateLogin(name, password){
+    
+}
+
 function connect(){
 	$.get("../api/getip", function(data) {
 		var jsondata = $.parseJSON(data);
@@ -69,8 +74,8 @@ function connect(){
 
 		if(ros_url != 1){
 			ros.socket.url = "ws://" + ros_url + ":9090";
-		}	
-	
+		}
+
 		var connected = false;
 		ros_call_service("/connection_service", "intelchair/ChairConnection", {connection: "c"}, function(result){
 			// console.log("Connection response: " + result.response);
@@ -85,10 +90,10 @@ function connect(){
 					setBatteryLabel(currentBattery);
 				});
 			}
-		});	
-	
-		
-		
+		});
+
+
+
 	});
 }
 
@@ -99,7 +104,7 @@ function velocityUp(){
 			currentSpeed++;
 			setSpeedLabel(currentSpeed);
 		});
-		
+
 	}
 }
 
@@ -135,7 +140,7 @@ function publish_info(topic, msg_type, data){
 		ros: ros,
 		name: topic,
 		messageType: msg_type
-		
+
 	});
 
 	publisher.publish(data);
@@ -148,7 +153,7 @@ function subscribe_info(topic, msg_type, callback){
 		messageType : msg_type
 	});
 
-	listener.subscribe(callback);		
+	listener.subscribe(callback);
 }
 
 function ros_call_service(service, service_type, data, callback){
@@ -160,7 +165,7 @@ function ros_call_service(service, service_type, data, callback){
 
 	var request = new ROSLIB.ServiceRequest(data);
 
-	_service.callService(request, callback);	
+	_service.callService(request, callback);
 }
 
 
@@ -226,5 +231,3 @@ function map_joystickC(x, y){
 	motor_vel.L = y + nx(x);
 	return motor_vel;
 }
-
-
