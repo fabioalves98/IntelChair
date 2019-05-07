6c7155e57fb3ad95ef51559b061cd61f7e00438e
Marco Silva <masilva@ua.pt>
2019-05-07 11:51:35 +0100

    Chair history database. Pycache deleted
    Former-commit-id: 74d3f513b6786a83ad5b5687d38b3eaf9805fbcf

--- server/intelchair/__init__.py
+++ server/intelchair/__init__.py
@@ -23,6 +23,8 @@ def get_db(name):
             db = g._database = shelve.open("users.db")
         elif name == "chairs.db":
             db = g._database = shelve.open("chairs.db")
+        elif name == "history.db":
+            db = g._database = shelve.open("history.db")
     return db
 
 @app.teardown_appcontext
@@ -48,6 +50,7 @@ def teardown_db(exception):
 def index():
     return open(app.root_path + '/index.html').read()
 
+@app.route("/")
 @app.route("/auth.html")
 def auth():
     return open(app.root_path + '/auth.html').read()
@@ -160,8 +163,59 @@ class Chair(Resource):
         return '', 204
 
 
+############## Chairs Use History ##############
+class HistoryList(Resource):
+    def get(self):
+        shelf = get_db("history.db")
+        keys = list(shelf.keys())
+
+        histories = []
+
+        for key in keys:
+            histories.append(shelf[key])
+
+        return json.dumps(histories), 200
+
+    def post(self):
+        parser = reqparse.RequestParser()
+        
+        parser.add_argument('start', required=True)
+        parser.add_argument('end', required=True)
+        parser.add_argument('username', required=True)
+        parser.add_argument('chairId', required=True)
+
+        # Parse the arguments into an object
+        args = parser.parse_args()
+
+        shelf = get_db("history.db")
+        shelf[args['chairId']] = args
+
+        return {'message': 'History saved', 'data': args}, 201
+
+class History(Resource):
+    def get(self, chairId):
+        shelf = get_db("history.db")
+
+        # If the key does not exist in the data store, return a 404 error.
+        if not (chairId in shelf):
+            return {'message': 'Chair history not found', 'data': {}}, 404
+
+        return {'message': 'Chair history found', 'data': shelf[chairId]}, 200
+
+    def delete(self, chairId):
+        shelf = get_db("history.db")
+
+        # If the key does not exist in the data store, return a 404 error.
+        if not (chairId in shelf):
+            return {'message': 'Chair history not found', 'data': {}}, 404
+
+        del shelf[chairId]
+        return '', 204
 
 api.add_resource(UserList, '/users')
 api.add_resource(User, '/users/<string:username>')
 api.add_resource(ChairList, '/chairs')
 api.add_resource(Chair, '/chairs/<string:name>')
+api.add_resource(HistoryList, '/chairs/history')
+api.add_resource(History, '/chairs/history/<string:chairId>')
+
